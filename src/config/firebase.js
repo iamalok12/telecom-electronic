b024857a23793e6d7a6dd42d887c25fedc08efa6
Alok <alok.kumar@nagra.com>
2026-05-09 02:41:27 +0530

    feat: remove .env.example and hardcode Firebase configuration in firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,13 +6,13 @@ import { getAnalytics } from 'firebase/analytics';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDDS3l0Gmyx_Ym4xSmwv4OKinhOZca8mb0",
+  authDomain: "telecom-electronics.firebaseapp.com",
+  projectId: "telecom-electronics",
+  storageBucket: "telecom-electronics.firebasestorage.app",
+  messagingSenderId: "949451440107",
+  appId: "1:949451440107:web:51f6ba30bccc4a249d63ae",
+  measurementId: "G-3W1FVGL1MH"
 };
 
 // Initialize Firebase
